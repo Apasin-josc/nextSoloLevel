1edb426d453ead49943422c1669e15b7c6225607
omarscoppola <joseomar_coppola@hotmail.com>
2025-03-14 09:05:24 +0100

    creating a new employees table in prisma

--- 03-admin-todos/src/app/api/seed/route.ts
+++ 03-admin-todos/src/app/api/seed/route.ts
@@ -1,25 +1,24 @@
-import prisma from '@/lib/prisma';
-import { NextResponse, NextRequest } from 'next/server'
+import prisma from "@/lib/prisma";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
+  await prisma.todo.deleteMany(); // delete * from todos
 
-    await prisma.todo.deleteMany(); // delete * from todos
+  await prisma.todo.createMany({
+    data: [
+      { description: "tickets sprint 88", complete: true },
+      { description: "phoenix team" },
+      { description: "autodata" },
+      { description: "solera" },
+      { description: "que padre" },
+    ],
+  });
 
-    await prisma.todo.createMany({
-        data: [
-            {description: 'tickets sprint 88', complete: true},
-            {description: 'phoenix team'},
-            {description: 'autodata'},
-            {description: 'solera'},
-            {description: 'que padre'},
-        ]
-    });
-
-    /* //TODO: making a insertion to the db
+  /* //TODO: making a insertion to the db
     const todo = await prisma.todo.create({
         data: { description: 'Piedra del alma', complete: true}
     });
     console.log(todo); */
 
-  return NextResponse.json({ message: 'Seed Executed'});
+  return NextResponse.json({ message: "Seed Executed" });
 }
